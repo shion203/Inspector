6024cddad7e60d8d911c7661a404dff0ddb79880
shiopon <gshion203@gmail.com>
2025-06-07 12:11:18 +0900

    クラスの入れ子に対応

--- Inspector/Model/MainModel.cs
+++ Inspector/Model/MainModel.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 
 namespace MyApp.Model
 {
+    public class HogeClass2
+    {
+        public int HogeInt { get; set; }
+        public float HogeFloat { get; set; }
+        public string HogeString { get; set; } = "HogeString";
+
+        public ObservableCollection<int> HogeIntCollection { get; set; } = new ObservableCollection<int> { 1, 2, 3, 4, 5 };
+
+    }
+
     /// <summary>
     /// ルートにぶら下がっているクラスプロパティ
     /// </summary>
@@ -16,13 +26,20 @@ namespace MyApp.Model
         public float HogeFloat { get; set; }
         public string HogeString { get; set; } = "HogeString";
 
-        public ObservableCollection<int> HogeIntCollection = new ObservableCollection<int>();
+        public ObservableCollection<int> HogeIntCollection { get; set; } = new ObservableCollection<int> { 1, 2, 3, 4, 5 };
 
-        /*
+        public HogeClass2 Hoge2 { get; set; } = new HogeClass2
+        {
+            HogeInt = 100,
+            HogeFloat = 1.5f,
+            HogeString = "Hoge2String",
+            HogeIntCollection = new ObservableCollection<int> { 1, 2, 3 }
+        };
+
         /// <summary>
-        /// 更に入れ子…これバグりそうじゃない？
-        /// まぁ深い階層は打ち切ったりしてもいいか…
+        /// @todo:クラスの入れ子は無限ループになっちゃうので対処必要そう
         /// </summary>
+        /*
         public HogeClass Hoge2 { get; set; } = new HogeClass
         {
             HogeInt = 100,
@@ -55,7 +72,7 @@ namespace MyApp.Model
         public HogeEnum HogeEnumValue { get; set; } = HogeEnum.Two;
 
         // Collectionのケース
-        public ObservableCollection<int> HogeIntCollection { get; set; } = new ObservableCollection<int>();
+        public ObservableCollection<int> HogeIntCollection { get; set; } = new ObservableCollection<int> { 1, 2, 3, 4, 5 };
 
         /// <summary>
         /// クラスのケース
--- Inspector/ViewModel/PropertyViewModels.cs
+++ Inspector/ViewModel/PropertyViewModels.cs
@@ -30,60 +30,87 @@ namespace MyApp.ViewModel
     /// </summary>
     public class ClassPropertyViewModel : PropertyViewModelBase
     {
-
-    // ...（省略）...
-
-    public ClassPropertyViewModel(string name, object obj)
-        : base(name, obj)
-    {
-        Properties.Clear();
-
-        foreach (PropertyInfo prop in obj.GetType().GetProperties())
+        public ClassPropertyViewModel(string name, object obj)
+            : base(name, obj)
         {
-            if (prop.CanRead && prop.CanWrite)
+            Properties.Clear();
+
+            foreach (PropertyInfo prop in obj.GetType().GetProperties())
             {
-                var value = prop.GetValue(obj);
+                if (prop.CanRead && prop.CanWrite)
+                {
+                    var value = prop.GetValue(obj);
 
-                if (value == null)
-                {
-                    continue;
-                }
+                    if (value == null)
+                    {
+                        continue;
+                    }
 
-                // コレクション型かどうか判定（stringは除外）
-                if (typeof(System.Collections.IEnumerable).IsAssignableFrom(prop.PropertyType) && prop.PropertyType != typeof(string))
-                {
-                    Dispatcher.CurrentDispatcher.Invoke(() =>
+                    // コレクション型かどうか判定（stringは除外）
+                    if (typeof(System.Collections.IEnumerable).IsAssignableFrom(prop.PropertyType) && prop.PropertyType != typeof(string))
                     {
-                        Properties.Add(new CollectionPropertyViewModel(prop.Name, value));
-                    });
-                }
-                else if (prop.PropertyType.IsClass && prop.PropertyType != typeof(string))
-                {
-                    Dispatcher.CurrentDispatcher.Invoke(() =>
+                        Dispatcher.CurrentDispatcher.Invoke(() =>
+                        {
+                            Properties.Add(new CollectionPropertyViewModel(prop.Name, value));
+                        });
+                    }
+                    else if (prop.PropertyType.IsClass && prop.PropertyType != typeof(string))
                     {
-                        Properties.Add(new ClassPropertyViewModel(prop.Name, value));
-                    });
-                }
-                else
-                {
-                    Dispatcher.CurrentDispatcher.Invoke(() =>
+                        Dispatcher.CurrentDispatcher.Invoke(() =>
+                        {
+                            Properties.Add(new ClassPropertyViewModel(prop.Name, value));
+                        });
+                    }
+                    else
                     {
-                        Properties.Add(new PropertyViewModelBase(prop.Name, value));
-                    });
+                        Dispatcher.CurrentDispatcher.Invoke(() =>
+                        {
+                            Properties.Add(new PropertyViewModelBase(prop.Name, value));
+                        });
+                    }
                 }
             }
         }
-    }
 
-        public ObservableCollection<PropertyViewModelBase> Properties { get; set; } = new ObservableCollection<PropertyViewModelBase>();
+        public ObservableCollection<PropertyViewModelBase> Properties { get; set; } = new ();
     }
 
     public class CollectionPropertyViewModel : PropertyViewModelBase
     {
+        public ObservableCollection<PropertyViewModelBase> Properties { get; set; } = new ();
+
         public CollectionPropertyViewModel(string name, object value)
             : base(name, value)
         {
+            Properties.Clear();
 
+            if (value is System.Collections.IEnumerable enumerable && value.GetType() != typeof(string))
+            {
+                foreach (var item in enumerable)
+                {
+                    if (item == null)
+                        continue;
+
+                    var itemType = item.GetType();
+
+                    if (itemType.IsEnum)
+                    {
+                        Properties.Add(new EnumPropertyViewModel(itemType.Name, item));
+                    }
+                    else if (typeof(System.Collections.IEnumerable).IsAssignableFrom(itemType) && itemType != typeof(string))
+                    {
+                        Properties.Add(new CollectionPropertyViewModel(itemType.Name, item));
+                    }
+                    else if (itemType.IsClass && itemType != typeof(string))
+                    {
+                        Properties.Add(new ClassPropertyViewModel(itemType.Name, item));
+                    }
+                    else
+                    {
+                        Properties.Add(new PropertyViewModelBase(itemType.Name, item));
+                    }
+                }
+            }
         }
     }
 
